ef9ff0d9e6427bafe1479e2869fe537d9b1c0f36
Vamesix <validatevamesix@gmail.com>
2020-11-12 20:09:24 +0800

    Update order.py

--- order.py
+++ order.py
@@ -5,15 +5,17 @@ import shutil
 labels = []
 with open("data/training_labels.csv") as file:
     csv_file = csv.reader(file)
-    next(csv_file)
+    next(csv_file)  # skip header
     for row in csv_file:
         index = int(row[0])
-        label = row[1].replace("/", "_")  # escape slash
+        label = row[1].replace("/", "_")  # escape slash, needed for directory
 
+        # Make a new directory if a new label is encountered
         if label not in labels:
             labels.append(label)
             os.mkdir(f"data/training_data/{label}")
 
+        # Move file
         source_file = f"data/training_data/{index:>06}.jpg"
         target_path = f"data/training_data/{label}"
         shutil.move(source_file, target_path)
